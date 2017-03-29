714f03348bde1cded64138eb02065ad5d1fc8442
ngoc tuan nguyen <ngoctuan82@gmail.com>
2017-03-28 17:52:50 -0700

    Add checkin button on profile page, add setting for notifier as well

--- src/app/pages/pages-profile/profile.component.ts
+++ src/app/pages/pages-profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../app.service';
+import { Http, URLSearchParams, Response, Headers} from  "@angular/http";
 import { ZentomicAuthService} from '../../service/zentomic.auth';
+import { ZENTOMIC_SERVICE } from '../../zentomic.service';
 
 @Component({
   selector: 'lk-profile',
@@ -13,10 +15,33 @@ export class PagesProfileComponent implements OnInit {
 
   //data
   user: any = {};
-
-  constructor(private appService: AppService, private zentomicAuthService: ZentomicAuthService) {
+  notifier: any = {Fullname:"", Fone:"", Email:""};
+  events: any = {};
+  // link 
+  url_get_notifier: string = ZENTOMIC_SERVICE.get_notifier;
+  url_save_notifier: string = ZENTOMIC_SERVICE.create_notifier;
+  url_checkin: string = ZENTOMIC_SERVICE.checkin;
+    //---------------------------------------------------------------------------------------------
+  constructor(private appService: AppService, private zentomicAuthService: ZentomicAuthService, private http: Http) {
     appService.getState().topnavTitle = 'Profile';
     this.loadUser();
+    this.loadNotifier();
+    var $this = this;
+    // event handler
+    this.events = {
+      
+      'save': function () {
+        $this.notifier.fromuser = $this.user.email;
+        $this.notifier.type = "sms";
+
+        //custom code here
+        $this.save();
+      },
+      'checkin': function () {
+        $this.checkin();
+      }
+    };
+
   }
 
   ngOnInit() {
@@ -33,5 +58,98 @@ export class PagesProfileComponent implements OnInit {
       this.user = d;
 
  
+  }
+
+  loadNotifier = () => {
+    //get notifier of current user
+    console.log("load notifier");
+    var url = this.url_get_notifier;
+    var cred = "?fromuser={{fromuser}}";
+    url = url + cred.replace("{{fromuser}}", this.user.email);
+
+    var response = this.http.get(url);
+
+    response
+      .map(response => response.json())
+      .subscribe(data => {
+        // we've got back the raw data, now generate the core schedule data
+        // and save the data for later reference
+
+        console.log(data);
+
+        //-----------
+        if (!(data == null)) {
+          //custom code
+          this.notifier = data.data.Info;
+        }
+      });
+  }
+  save = () => {
+    console.log("click save");
+    var url = this.url_save_notifier;
+    var cred = "fromuser={{fromuser}}&fullname={{fullname}}&fone={{fone}}&email={{email}}&type={{type}}";
+    cred = cred.replace("{{fromuser}}", this.user.email);
+    cred = cred.replace("{{fullname}}", this.notifier.Fullname);
+    cred = cred.replace("{{fone}}", this.notifier.Fone);
+    cred = cred.replace("{{email}}", this.notifier.Email);
+    cred = cred.replace("{{type}}", this.notifier.type);
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    var response = this.http.post(url, cred, {
+      headers: headers
+    });
+
+    response
+      .map(response => response.json())
+      .subscribe(data => {
+        // we've got back the raw data, now generate the core schedule data
+        // and save the data for later reference
+        
+        console.log(data);
+
+        //-----------
+        if (!(data == null)) {
+          //custom code
+          
+        }
+      });
+  }
+
+
+  checkin = () => {
+    //get notifier of current user
+    console.log("check in");
+    var url = this.url_checkin;
+    var cred = "?email={{email}}";
+    url = url + cred.replace("{{email}}", this.user.email);
+
+    var response = this.http.get(url);
+
+    response
+      .map(response => response.json())
+      .subscribe(data => {
+        // we've got back the raw data, now generate the core schedule data
+        // and save the data for later reference
+
+        console.log(data);
+
+        //-----------
+        if (!(data == null)) {
+          //custom code
+          this.notifier = data.data.Info;
+        }
+      });
+  }
+
+  /**
+ * events trigger handler
+ * @param event
+ */
+  onEvent(event) {
+    console.log("click " + event);
+    if (this.events) {
+      this.events[event]();// true handler here please jump to it
+    }
   }
 }
--- src/app/zentomic.service.ts
+++ src/app/zentomic.service.ts
@@ -3,14 +3,17 @@
  * Code by Tony Nguyen
  */
 
-//const ZENTOMIC_SERVER = "http://localhost:9000";
-const ZENTOMIC_SERVER = "https://zentomic-notifier-node.herokuapp.com";
+const ZENTOMIC_SERVER = "http://localhost:9000";
+//const ZENTOMIC_SERVER = "https://zentomic-notifier-node.herokuapp.com";
 
 export const ZENTOMIC_SERVICE = {
   login_local: ZENTOMIC_SERVER + "/login-local?email={{email}}&password={{password}}",
   login_google: ZENTOMIC_SERVER + '/auth/google/callback',
   sign_up: ZENTOMIC_SERVER + '/signup-local',
   SMS: ZENTOMIC_SERVER + '/SMS',
+  create_notifier: ZENTOMIC_SERVER + '/create_notifier',
+  get_notifier: ZENTOMIC_SERVER + '/get_notifier',
+  checkin: ZENTOMIC_SERVER + '/checkin',
 };
 
 
